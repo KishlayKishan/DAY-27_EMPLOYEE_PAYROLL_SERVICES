59fec94ddba8908bd6c289485fe226b007fe1832
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-24 12:49:25 +0530

    Added:Watch Service to watch particular directory along with all Files and Sub Directories

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayRollService.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayRollService.java
@@ -5,39 +5,20 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayRollService {
-	private static List<EmployeePayRoll> empPayRollList;
-	private static Scanner sc = new Scanner(System.in);
+	private List<EmployeeData> employePayrollList = new ArrayList<EmployeeData>();
+	Scanner scanner = new Scanner(System.in);
 
-	public EmployeePayRollService(List<EmployeePayRoll> empPayRollList) {
-		EmployeePayRollService.empPayRollList = empPayRollList;
+	public void readEmployeeDataFromConsole() {
+		System.out.println("Enter Employee Id");
+		int id = scanner.nextInt();
+		System.out.println("Enter Employee Name");
+		String Name = scanner.next();
+		System.out.println("Enter the salary");
+		int salary = scanner.nextInt();
+		employePayrollList.add(new EmployeeData(id, Name, salary));
 	}
 
-	public static void main(String[] args) {
-		ArrayList<EmployeePayRoll> empPayRollList = new ArrayList<EmployeePayRoll>();
-		EmployeePayRollService empPayRollService = new EmployeePayRollService(empPayRollList);
-
-		EmployeePayRollService.readData();
-
-		empPayRollService.writeData();
-
+	public void writeEmployeeDataInConsole() {
+		System.out.println("Writing Employee Pay Roll Data \n" + employePayrollList);
 	}
-
-	private static void readData() {
-		System.out.println("Please Enter the following details :");
-		System.out.println("Enter ID");
-		int id = sc.nextInt();
-		sc.nextLine();
-		System.out.println("Enter Name :");
-		String name = sc.nextLine();
-		System.out.println("Enter Salary :");
-		double salary = sc.nextDouble();
-
-		EmployeePayRoll empPayRollObject = new EmployeePayRoll(id, name, salary);
-		empPayRollList.add(empPayRollObject);
-	}
-
-	private void writeData() {
-		System.out.println("Employee Pay Roll Data : \n" + empPayRollList.get(0).toString());
-	}
-
 }
