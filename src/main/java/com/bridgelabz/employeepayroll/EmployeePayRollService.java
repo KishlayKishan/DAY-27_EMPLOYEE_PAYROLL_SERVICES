21fbcbcab404e338f206674072c8f00f6b6e8080
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-24 18:09:34 +0530

    Added:Employee Payroll Service to show number of Entries

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayRollService.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayRollService.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class EmployeePayRollService {
 	private List<EmployeeData> employePayrollList = new ArrayList<EmployeeData>();
 	Scanner scanner = new Scanner(System.in);
-	private static final String FILE_PATH = "C:\\Users\\HP\\Desktop\\data.txt";
+	private static final String FILE_PATH = "c://Users//malij//OneDrive//Desktop//payroll-file.txt";
 
 	public void readEmployeeDataFromConsole() {
 		System.out.println("Enter Employee Id");
@@ -41,17 +41,18 @@ public class EmployeePayRollService {
 		try {
 			Files.write(Paths.get(FILE_PATH), empBuffer.toString().getBytes());
 		} catch (IOException e) {
-
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
+	// method to create file if file doesn't exist
 	private void checkFile() {
 		File file = new File(FILE_PATH);
 		try {
-
+			// checking file already exists
 			if (!file.exists()) {
-
+				// if not creating a new file
 				file.createNewFile();
 				System.out.println("Created a file at " + FILE_PATH);
 			}
@@ -60,6 +61,7 @@ public class EmployeePayRollService {
 		}
 	}
 
+	// method to count entries
 	public long countEntries() {
 		long entries = 0;
 		try {
@@ -68,5 +70,14 @@ public class EmployeePayRollService {
 			e.printStackTrace();
 		}
 		return entries;
+	}
+
+	// method to print content of file
+	public void printData() {
+		try {
+			Files.lines(Paths.get(FILE_PATH)).forEach(System.out::println);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
