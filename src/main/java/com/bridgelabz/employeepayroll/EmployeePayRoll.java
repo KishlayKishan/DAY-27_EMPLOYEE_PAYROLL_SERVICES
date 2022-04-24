b46337757c858064c8945dd9917bd6036b779e3a
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-24 12:17:08 +0530

    Added:Employee Payroll Service to Read and Write Employee Payroll to a Console

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayRoll.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayRoll.java
@@ -3,7 +3,7 @@ package com.bridgelabz.employeepayroll;
 public class EmployeePayRoll {
 
 	public EmployeePayRoll(int id, String name, double salary) {
-		// TODO Auto-generated constructor stub
+
 	}
 
 }
