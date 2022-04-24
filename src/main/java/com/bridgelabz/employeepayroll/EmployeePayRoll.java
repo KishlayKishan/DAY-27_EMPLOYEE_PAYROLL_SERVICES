fd67c7bfc5e1dec4349862fca2b285b53a66cdfd
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-24 12:12:51 +0530

    Added:Git Ignore Files.

---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayRoll.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeepayroll;
+
+public class EmployeePayRoll {
+
+	public EmployeePayRoll(int id, String name, double salary) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
